7a29d79b8178cde8477d0c2d1a512bc728a1795e
Andrew <dragondogpro@gmail.com>
2022-03-14 17:46:05 +0500

    Added character movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private CharacterController _controller;
+    [SerializeField] private float Gravity = 0.0f;
+
+    private Vector3 _direction;
+
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (_controller.isGrounded)
+            _direction = new Vector3(horizontal, 0f, vertical).normalized;
+        else
+            _direction.y -= Gravity * Time.deltaTime;
+
+        if (_direction.magnitude >= 0.1f)
+            _controller.Move(_direction * _moveSpeed * Time.deltaTime);
+    }
+}
